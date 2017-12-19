45222944bff7afd459c5e8cbe838b5f07b0c30f8
Berkat Bhatti <beme@theknightmarket.com>
2017-12-19 13:41:09 -0500

    Map VC ui implimented

---
+++ pixCity/Controller/MapVc.swift
@@ -0,0 +1,36 @@
+//
+//  MapVc.swift
+//  pixCity
+//
+//  Created by berkat bhatti on 12/19/17.
+//  Copyright © 2017 TKM. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapVc: UIViewController {
+    
+//---Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+//--Variables and Arrays
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }//end view did load
+
+//--Protocol Related Functions
+    
+    
+//---Actions
+    
+//---Gestures and Animations
+    
+//---Selectors
+    
+//---View update Function
+   
+}//end controller
